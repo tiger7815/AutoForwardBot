2a275448ba34ad0debbdda05b1d3108939c7ea11
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2023-12-25 04:02:51 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,15 +1,12 @@
 class Translation(object):
 
       # Start text
-      START = "<b>Hi 👋 I Am A Channel Auto Forward Bot of <a href='https://t.me/VJ_Botz'>VJ Botz</a><b>. \n<b>Make your own bot now 👉 <a href='https://youtube.com/@Tech_VJ'>Tutorial Video</a><b> \n\n<b>Support Channel and Groups:</b>\n @VJ_Bots, @VJ_Bot_disscussion \n\n<b>About Bot:</b>\n Give Command /about"             
+      START = "<b>Hi 👋 I Am A Channel Auto Forward Bot"             
 
       #About text
       ABOUT = """<b>⍟───[ MY ᴅᴇᴛᴀɪʟꜱ ]───⍟
-‣ ᴍʏ ɴᴀᴍᴇ : <a href=https://t.me/VJ_Botz>VJ Auto Forward Bot 🇮🇳</a>
+‣ ᴍʏ ɴᴀᴍᴇ :  Auto Forward Bot 🇮🇳</a>
  ‣ My best friend : <a href='tg://settings'>ᴛʜɪs ᴘᴇʀsᴏɴ</a> 
- ‣ ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href='https://t.me/kingvj01'>Tech VJ 🇮🇳</a> 
+ ‣ ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href='https://t.me/don_owner'> SiD Op 🇮🇳</a> 
  ‣ ʟɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>ᴘʏʀᴏɢʀᴀᴍ</a> 
- ‣ ʟᴀɴɢᴜᴀɢᴇ : <a href='https://www.python.org/download/releases/3.0/'>ᴘʏᴛʜᴏɴ 3</a> 
- ‣ ᴅᴀᴛᴀ ʙᴀsᴇ : <a href='https://www.mongodb.com/'>ᴍᴏɴɢᴏ ᴅʙ</a> 
- ‣ ʙᴏᴛ sᴇʀᴠᴇʀ : <a href='https://render.com'>Render</a> 
  ‣ ʙᴜɪʟᴅ sᴛᴀᴛᴜs : ᴠ2.7.1 [sᴛᴀʙʟᴇ]></b>"""
